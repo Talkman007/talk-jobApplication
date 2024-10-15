6c2b3968b53b02eae1944cb88ac11b69670065a1
Talkman <156525865+Talkman007@users.noreply.github.com>
2024-10-15 15:00:13 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config = {
             ped = 's_m_y_cop_01',
             location = vector3(442.77053, -981.9479, 29.689575),
             heading = 90.0,
-            webhook = 'https://discord.com/api/webhooks/1293295265788461149/7mKriyA-pIbivCYaQe19Wq2gasKJta08rkwdKKpnZRohP-awIkaJyJzeLYZvXEoRusU-'
+            webhook = ''
         },-- You Can Add More IF You Need! For Example:
         --{
         --    job = 'mechanic',
